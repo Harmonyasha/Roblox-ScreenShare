fc5ef1ddb9e07d6df35f5324e7e5e6937c709aad
OwlUniversal <71132236+OwlUniversal@users.noreply.github.com>
2024-01-13 23:34:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,35 +2,71 @@ import time
 from PIL import ImageGrab, ImageOps
 from flask import Flask, jsonify, request, render_template
 import ngrok_library
-app = Flask(__name__,static_url_path='') #/cool2
+import threading
+arr = {}
+frame = 20
+app = Flask(__name__,static_url_path='')
 ngrok_library.run_with_ngrok(app)
+
+
+def recordimage():
+    global arr
+    position = arr.__len__()
+    arr[position] = "nil"
+    t = time.time()
+    sizes = open("monitor_size.txt","r")
+    sizes = sizes.readline()
+    sizes = sizes.split("x")
+    snapshot = ImageGrab.grab()
+    snapshot = snapshot.resize((int(sizes[0]),int(sizes[1])))
+    snapshot = ImageOps.mirror(snapshot)
+    pixels = snapshot.load()
+    width, height = snapshot.size
+    snapshot.close()
+    all_pixels = []
+    all_pixels.append(f"video;{str(width)}x{str(height)};")
+    for x in range(width):
+        for y in range(height):
+            cpixel = pixels[x, y]
+            cpixel = str(cpixel)
+            cpixel = cpixel.replace("(","").replace(")","")
+            all_pixels.append(cpixel+";")
+
+    arr[position] = (''.join(str(x) for x in all_pixels).replace(" ",""))
+    
+
+def check():
+    ret = True
+    try:
+        for v in arr:
+            if arr[v] == "nil":
+                ret = False
+    except: 
+        ret = False
+    return ret
+
 @app.route('/', methods=['POST'])
 def result():
-    if "getimg" in request.json:
-        t = time.time()
-        sizes = open("monitor_size.txt","r")
-        sizes = sizes.readline()
-        sizes = sizes.split("x")
-        snapshot = ImageGrab.grab()
-        snapshot = snapshot.resize((int(sizes[0]),int(sizes[1])))
-        snapshot = ImageOps.mirror(snapshot)
-        pixels = snapshot.load()
-        width, height = snapshot.size
-        all_pixels = []
-        all_pixels.append(f"video;{str(width)}x{str(height)};")
-        for x in range(width):
-            for y in range(height):
-                cpixel = pixels[x, y]
-                cpixel = str(cpixel)
-                cpixel = cpixel.replace("(","").replace(")","")
-                all_pixels.append(cpixel+";")
-        snapshot.close()
-        print(f"sended for {time.time()-t}")
-        return ''.join(str(x) for x in all_pixels)
-    return "Invalid method"
+    global arr 
+    #while not check():
+    #    None
+    clonearr = arr
+    arr = {}
+    return clonearr
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    global arr 
+    arr = {}
+    return "True"
+
+def flaskrun():
+    app.run(token = "YourNgrokToken",domain = "Create domain if you want")
+
+th = threading.Thread(target=flaskrun)
+th.start()
 
 
-if __name__ == "__main__":
-  app.run(token = "YourNgrokToken",domain = "Create domain if you want")
-
-    
+while True:
+        threading.Thread(target=recordimage).start()
+        time.sleep(.1)
